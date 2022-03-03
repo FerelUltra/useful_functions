8b09df875f360641093dbfac6156589cd6006732
FeralDaro <meral.nekrag@gmail.com>
2022-03-03 11:56:57 +0300

    Initial commit

---
+++ elementOutsideEvent.js
@@ -0,0 +1,21 @@
+function useOutsideAlerter(ref) {
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        //write logic
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+}
+const wrapperRef = useRef(null);
+useOutsideAlerter(wrapperRef);
